2b1d7017e1f75f8c3e2181eea3e175aecac7ba2f
Prathima-Sri <153510461+Prathima-Sri@users.noreply.github.com>
2023-12-15 10:20:06 +0530

    Update Even.java

--- Even.java
+++ Even.java
@@ -1,9 +1,10 @@
 public class Even {
     public static void main(String[] args) {
-        System.out.println("Even numbers from 1 to 28:");
+        System.out.println("Even numbers from 1 to 30:");
 
-        for (int i = 2; i <= 28; i += 2) {
+        for (int i = 2; i <= 30; i += 2) {
             System.out.println(i);
+            System.out.println("28th Birthday");
         }
     }
 }
